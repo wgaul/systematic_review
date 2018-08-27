bf06603ed076264fe809d2e348fa63dc3d2bd02e
wgaul <wgaul@hotmail.com>
2018-08-27 16:49:41 +0100

    Correcting obvious coding errors revealed by double coding.

--- clean_data_syst_rev.R
+++ clean_data_syst_rev.R
@@ -1,14 +1,141 @@
 ###########################################
 ## Clean systematic review data
 ## 
-## inputs:
+## inputs:  * master_eligibility_results.csv - a csv with eligibility coding
+##          * systematic_review_coded_results.csv - wg's original coding (not
+##              corrected based on comparison to second reader)
+##          * casey_coded_results.csv - cp's codings after the first 10 or 11
+##              article callibration period.  This includes corrections entered
+##              after the callibration talk.
+##          * casey_coded_results_pre_calibration.csv - cp's original coding of
+##              callibration articles before calibration talk and re-evaluation
 ## 
-## outputs:
+## outputs: * elig - the original eligibility scoring
+##          * rev - wg's coding of articles after correcting errors revealed by
+##              double coding. This is the object to use.
+##          * rev_orig - wg's original coding of articles before correcting 
+##              errors revealed by double coding.  For use only in calculating
+##              agreement statistics.
+##          * cp - Casey's codings after the calibration discussion and after 
+##              correcting obvious errors based on comparison to wg's coding. 
+##              This is the object to use.
+##          * cp_orig - Casey's codings after the calibration distcussion but 
+##              before correcting obvious errors based on comparison to wg's 
+##              coding.  For use only in calculating agreement statistics.
+##          * cp_pre_cal - Casey's codings for the 10 or 11 articles before the
+##              calibration meeting.
 ## 
-## notes:
+## notes: This script is where all corrections are made based on errors revealed
+##  in double coding.
 ## 
 ## author: Willson Gaul wgaul@hotmail.com
 ## created: 27 Aug 2018
 ## last modified: 27 Aug 2018
 ###########################################
+
+setwd("~/Documents/Data_Analysis/UCD/systematic_review/")
+
+# read in data
+elig <- read_csv("~/Documents/UCD/PhD_Project/systematic_review/master_eligibility_results.csv")
+rev_orig <- read_csv("./data/systematic_review_coded_results.csv")
+cp_orig <- read_csv("./data/casey_coded_results.csv")
+cp_pre_cal <- read_csv("./data/casey_coded_results_pre_calibration.csv")
+
+## remove columns which are used as visual separators with no data ------------
+# wg data
+if(any(grepl("X.*", colnames(rev_orig)))) {
+  rev_orig <- rev_orig[, -which(grepl("X.*", colnames(rev_orig)))]
+}
+colnames(rev_orig) <- make.names(colnames(rev_orig))
+
+# casey's pre-calibration coding
+if(any(grepl("X.*", colnames(cp_pre_cal)))) {
+  cp_pre_cal <- cp_pre_cal[, -which(grepl("X.*", colnames(cp_pre_cal)))]
+}
+colnames(cp_pre_cal) <- make.names(colnames(cp_pre_cal))
+
+# casey's post-calibration coding
+if(any(grepl("X.*", colnames(cp_orig)))) {
+  cp_orig <- cp_orig[, -which(grepl("X.*", colnames(cp_orig)))]
+}
+colnames(cp_orig) <- make.names(colnames(cp_orig))
+
+
+## tidy some column names manually --------------------------------------------
+# wg data
+colnames(rev_orig)[which(
+  colnames(rev_orig) == 
+    "X.if.prediction.used..prediction.performance.measure")] <- 
+  "predicton.performance.measure"
+
+# casey pre-calibration data
+colnames(cp_pre_cal)[which(
+  colnames(cp_pre_cal) == 
+    "X.if.prediction.used..prediction.performance.measure")] <- 
+  "predicton.performance.measure"
+
+# casey post-callibration data
+colnames(cp_orig)[which(
+  colnames(cp_orig) == 
+    "X.if.prediction.used..prediction.performance.measure")] <- 
+  "predicton.performance.measure"
+
+## subset to qualifying articles for which coding is done ---------------------
+# wg data
+rev_orig <- rev_orig[which(rev_orig$coding.DONE == T), ] 
+rev_orig <- rev_orig[which(rev_orig$qualifies == T), ]
+rev_orig <- rev_orig[, -which(colnames(rev_orig) %in% c(
+  "link", "inclusion.order", "other.notes", 
+  "aspects.not.yet.coded", 
+  "eligible.for.meta..or.publication.bias.analysis", 
+  "wg.needs.to.read"))]
+
+# casey post-callibration data
+cp_orig <- cp_orig[, -which(colnames(cp_orig) %in% c("other.notes"))]
+cp_pre_cal <- cp_pre_cal[, -which(colnames(cp_pre_cal) %in% 
+                                    c("other.notes"))]
+
+## correct errors revealed by double coding -----------------------------------
+# This fixes things that were obvioulsy oversights or errors in coding that were
+# revealed by comparing 2 readers' codings.  Things that are legitimately 
+# ambiguous are not changed.  Original codings are preserved so that agreement
+# statistics can be calculated for before and after correction.  This will 
+# inform whether all articles and variables should be double-coded.
+
+# wg codings
+rev <- rev_orig
+rev[grep(".*Nature protection areas of Europe.*", rev$title), 
+    "qualifies"] <- FALSE
+rev[grep("Explaining European fungal fruiting.*", rev$title), 
+    "data.type...museum"] <- TRUE
+rev[grep("British phenological records indicate high diversity.*", rev$title), 
+    "data.type...sampling.effort.reported"] <- TRUE
+
+
+# cp codings
+cp <- cp_orig
+cp[grep("Explaining European fungal fruiting.*", cp$title), 
+   "results.type...inference"] <- TRUE
+cp[grep("Ocean current conn.*", cp$title), "results.type...inference"] <- FALSE
+cp[grep("Congruency in fungal phen.*", cp$title), "data.type...museum"] <- TRUE
+cp[grep("Evaluating promotional app.*", cp$title), 
+   "data.type...organized.data.collection.scheme"] <- FALSE
+cp[grep(".*assessment of bumblebee.*land use.*", cp$title), 
+   "biological.records.in.facilitative.role.but.not.analyzed"] <- FALSE
+cp[grep("Modelling and mapping UK emissions.*", cp$title), 
+   "biological.records.in.facilitative.role.but.not.analyzed"] <- FALSE
+cp[grep("The role of historical environmental.*", cp$title), 
+   "biological.records.in.facilitative.role.but.not.analyzed"] <- FALSE
+cp[grep("Evaluating promotional app.*", cp$title), 
+   "data.type...sampling.effort.reported"] <- FALSE
+cp[grep("Large reorganizations in butterfly.*", cp$title), 
+   "data.type...sampling.effort.reported"] <- TRUE
+
+## end correct errors ---------------------------------------------------------
+
+
+## drop any newly disqualified studies ----------------------------------------
+# these are the objects to use for analysis
+rev <- rev[which(rev$coding.DONE == T & rev$qualifies == T), ]
+cp <- cp[which(cp$coding.DONE == T & cp$qualifies == T), ]
 
