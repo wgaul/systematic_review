440d402f67174dd90efde10d26f8c607fc2b1460
wgaul <wgaul@hotmail.com>
2018-08-27 12:32:10 +0100

    Separate script for data cleaning.

---
+++ clean_data_syst_rev.R
@@ -0,0 +1,14 @@
+###########################################
+## Clean systematic review data
+## 
+## inputs:
+## 
+## outputs:
+## 
+## notes:
+## 
+## author: Willson Gaul wgaul@hotmail.com
+## created: 27 Aug 2018
+## last modified: 27 Aug 2018
+###########################################
+
